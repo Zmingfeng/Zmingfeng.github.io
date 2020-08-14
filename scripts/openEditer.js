05aee051031ba5d06c48e666387b2df4cdd5a667
Zmingfeng <xycg.sky@gmail.com>
2020-08-14 16:25:36 +0800

    add RomanToInteger

--- scripts/openEditer.js
+++ scripts/openEditer.js
@@ -1,4 +1,4 @@
 var spawn = require('child_process').exec;
 hexo.on('new', function(data){
-  spawn('start  "D:\Program File\Typora\Typora.exe" ' + data.path);
+  spawn('start  "E:\Program File\Typora\Typora.exe" ' + data.path);
 });
